00707908b6d71f378da8749067d9fd462451c2a2
Pragadesh BS <71137576+PragadeshBS@users.noreply.github.com>
2020-11-07 09:08:19 +0530

    Update LoginHandler.py
    Added a feature to append new data to existing data

--- LoginHandler.py
+++ LoginHandler.py
@@ -37,6 +37,7 @@ def login_handler(current_user, current_user_password, file_name, relaunch=False
     if old_data == "":
         print("'a' - add data to your account")
     else:
+        print("'add' - append new data to your existing data")
         print("'m' - modify your data")
         print("'c' - clear your data")
     print("'ch' - change your password")
@@ -44,25 +45,32 @@ def login_handler(current_user, current_user_password, file_name, relaunch=False
     print("'s' - logout and sign in/sign up with a different account")
     print("'delete' - delete your account\n")
     while True:
-        user_input = input(">>").lower().strip()
-        if (user_input == "m") or (user_input == "a"):
-            new_data = input("Enter new data>>")
+        user_input = input(" >>").lower().strip()
+        if (old_data != "") and (user_input == "add"):
+            append_data = input("Enter data to add >>")
+            while Check.data(append_data) != 1:
+                append_data = input("Enter new data >>")
+            else:
+                Dh.append_data(current_user, current_user_password, file_name, append_data)
+                login_handler(current_user, current_user_password, file_name, True)
+        elif (user_input == "m") or (user_input == "a"):
+            new_data = input("Enter new data >>")
             while Check.data(new_data) != 1:
-                new_data = input("Enter new data>>")
+                new_data = input("Enter new data >>")
             else:
                 Dh.write_data(current_user, current_user_password, new_data, file_name)
                 login_handler(current_user, current_user_password, file_name, True)
             break
         elif user_input == "ch":
-            confirmation = input("Enter your current password to proceed>>")
+            confirmation = input("Enter your current password to proceed >>")
             if confirmation == current_user_password:
-                new_pwd = input("\nEnter new password>>")
+                new_pwd = input("\nEnter new password >>")
                 while (Check.password(new_pwd, current_user) != 1) or (new_pwd == current_user_password):
                     if new_pwd == current_user_password:
                         print("Your new password is similar to your current password, try a different one")
-                    new_pwd = input("Enter new password>>")
+                    new_pwd = input("Enter new password >>")
                 else:
-                    confirm_new_pwd = input("Re-enter your new password>>")
+                    confirm_new_pwd = input("Re-enter your new password >>")
                     if confirm_new_pwd == new_pwd:
                         Ch.change_password(current_user, current_user_password, new_pwd, file_name)
                         login_handler(current_user, new_pwd, file_name, True, 1)
@@ -72,7 +80,7 @@ def login_handler(current_user, current_user_password, file_name, relaunch=False
                 login_handler(current_user, current_user_password, file_name, True, -2)
         elif user_input == "delete":
             print("\nYou are about to PERMANENTLY DELETE your account and related data, are you sure?")
-            confirmation = input("Enter your password to proceed>>")
+            confirmation = input("Enter your password to proceed >>")
             if confirmation == current_user_password:
                 Ch.remove_credentials(current_user, current_user_password, file_name)
                 root.clear()
@@ -81,14 +89,17 @@ def login_handler(current_user, current_user_password, file_name, relaunch=False
             else:
                 login_handler(current_user, current_user_password, file_name, True, delete=-1)
         elif user_input == "c":
-            confirmation = input("This will clear all your data, are you sure? (y/n)>>").lower().strip()
+            confirmation = input("This will clear all your data, are you sure? (y/n) >>").lower().strip()
             if (confirmation == "y") or (confirmation == "yes"):
                 Dh.write_data(current_user, current_user_password, "", file_name)
                 login_handler(current_user, current_user_password, file_name, True, clear_data=1)
                 break
-            else:
+            elif (confirmation == "n") or (confirmation == "no"):
                 login_handler(current_user, current_user_password, file_name, True, clear_data=-1)
                 break
+            else:
+                print("That was not expected, your data was not cleared")
+                continue
         elif user_input == "exit":
             sys.exit()
         elif user_input == "s":
